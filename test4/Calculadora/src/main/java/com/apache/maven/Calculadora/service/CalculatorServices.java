85f691a34470ecae46b58c740ce2d66b32c77be3
NicolaGernone <nicola.gernone02@gmail.com>
2019-12-18 05:05:01 +0100

    Creación servicios de calculo

--- test4/Calculadora/src/main/java/com/apache/maven/Calculadora/service/CalculatorServices.java
+++ test4/Calculadora/src/main/java/com/apache/maven/Calculadora/service/CalculatorServices.java
@@ -1,5 +1,28 @@
 package com.apache.maven.Calculadora.service;
 
+import org.springframework.stereotype.Service;
+
+import com.apache.maven.Calculadora.bean.OperationBean;
+
+@Service
 public class CalculatorServices {
 
+	
+	/**
+	 * servicio que devuelve la suma de dos valores
+	 * @param form
+	 * @return
+	 */
+	private int add(OperationBean form){
+        return form.getA() + form.getB();
+    }
+	
+	/**
+	 * servicio que devuelve la resta de dos valores
+	 * @param form
+	 * @return
+	 */
+    private int subtract(OperationBean form){
+        return form.getA() - form.getB();
+    }
 }
