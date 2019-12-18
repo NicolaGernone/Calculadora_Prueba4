4d4737475d5d3674816a625109ea3b15f212ba09
NicolaGernone <nicola.gernone02@gmail.com>
2019-12-18 05:35:41 +0100

    inicializo el controlador y mapeo las request a los servicios de la calculadora. agregacion de dependencies de spring para servicios rest al pom

--- test4/Calculadora/src/main/java/com/apache/maven/Calculadora/controller/Controller.java
+++ test4/Calculadora/src/main/java/com/apache/maven/Calculadora/controller/Controller.java
@@ -1,5 +1,27 @@
 package com.apache.maven.Calculadora.controller;
 
+import com.apache.maven.Calculadora.service.CalculatorServices;
+import com.apache.maven.Calculadora.bean.OperationBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class Controller {
+	
+	//inicializo el bean
+	OperationBean form = new OperationBean();
+	
+	//me conecto a los servicios
+	@Autowired
+	private CalculatorServices calculatorServices;
+	
+	@RequestMapping("/calculator")
+    public String getCalculatorPage(Model model){
+        model.addAttribute("operationBean",form);
+        return "calculator";
+	}
+	
+	
 
 }
